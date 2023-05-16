c64b137b1e3d43ee2bc7f86334b34d391665b75f
Suman Maity <87299903+SumanMaity9748@users.noreply.github.com>
2023-05-16 12:31:42 +0530

    Create re_findall.py

---
+++ re_findall.py
@@ -0,0 +1,11 @@
+import re
+
+cons = 'QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm'
+vowels = 'AEIOUaeiou'
+
+if __name__ == "__main__":
+    string = input().strip()
+    
+    m = re.findall(r"(?<=[%s])([%s]{2,})[%s]" % (cons, vowels, cons), string)
+    
+    print("\n".join(m or ['-1']))
