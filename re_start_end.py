f0e7c398d72cd2a35c990b5c5712b9515d9cd8a5
Suman Maity <87299903+SumanMaity9748@users.noreply.github.com>
2023-05-16 12:34:53 +0530

    Create re_start_end.py

---
+++ re_start_end.py
@@ -0,0 +1,13 @@
+import re
+
+if __name__ == "__main__":
+    string = input()
+    sub = input()
+    
+    matches = list(re.finditer(r'(?={})'.format(sub), string))
+    
+    if matches:
+        for match in matches:
+            print((match.start(), match.end() + len(sub) - 1))
+    else:
+        print((-1, -1))
